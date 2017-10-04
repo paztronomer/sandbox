453a45817030ee9666d0aaf3acdf9dddd4f1a804
Francisco Paz-Chinchon <fpazch@illinois.edu>
2017-10-04 16:19:28 -0500

    saves header header extensions in a list to be used

--- FITS_headerInfo.py
+++ FITS_headerInfo.py
@@ -5,6 +5,7 @@ import socket
 import argparse
 import logging
 import uuid
+import copy
 import numpy as np
 import multiprocessing as mp
 import fitsio
@@ -12,12 +13,16 @@ import fitsio
 
 class FitsImage():
     def __init__(self, fpath=None, keys=None, outnm=None):
+        # In the future, change this dictionary for something more elegant
+        # fpath, keys, outnm = kdic["fpath"], kdic["keys"], kdic["outnm"]
         f = fitsio.FITS(fpath)
         # x_header = fitsio.read_header(fpath)
-        x_head = f[0].read_header()
-        x_hdu = f[0].read()
-        self.ccd = np.copy(x_hdu)
-        self.header = x_head
+        # x_head = f[0].read_header()
+        # x_hdu = f[0].read()
+        # self.ccd = np.copy(x_hdu)
+        # self.header = x_head
+        H = [x.read_header() for x in f]
+        self.header = H
         if (outnm is None):
             self.out = str(uuid.uuid4()) + ".csv"
         else:
@@ -26,12 +31,23 @@ class FitsImage():
             else:
                 self.out = outnm + ".csv"
         f.close()
+    
+    def toy(self, x):
+        a = np.log(x*np.pi*1E45)
 
     def get_kw(self):
         """ To get the keywords and contruct the output file
         """
-        for i in range(100):
-            print self.header
+        for idx, ext in enumerate(self.header):
+            if (idx > 0):
+                print ext["DATASECA"]
+
+        proc_lst = []
+        for i in range(10):
+            proc = mp.Process(target=self.toy, args=(2,))
+            proc_lst.append(proc)
+        [x.start() for x in proc_lst]
+        [x.join() for x in proc_lst]
 
 if __name__ == "__main__":
     # If file output, then use filename="some.txt" as argument
@@ -51,15 +67,22 @@ if __name__ == "__main__":
     eel.add_argument("-o", help=t4, metavar="")
     # t4 = "Number of processes to be started (multiprocessing). Default: 4"
     # eel.add_argument("-p", help=t4, metavar="", default=4, type=int)
-    nn = eel.parse_args
+    nn = eel.parse_args()
+    # Initialize
     d = dict()
     d["fpath"] = nn.fits
     d["keys"] = nn.keyword
+    d["outnm"] = nn.o
 
-    # Initialize
+    F = FitsImage(**d)
+    F.get_kw()
 
     # Set multi-workers, process or what I need
-    proc = mp.Process(target=FitsImage, args=(kwargs=d))
-    proc.start()
-    proc.join()
+    # d = mp.Manager().dict()
+    # d["fpath"] = nn.fits
+    # d["keys"] = nn.keyword
+    # d["outnm"] = nn.o
+    # proc = mp.Process(target=FitsImage, args=(d,))
+    # proc.start()
+    # proc.join()
 
